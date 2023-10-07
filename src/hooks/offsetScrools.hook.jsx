06abd191c9716009b50c7854fa7f18b886393195
jonathan gleyze <jonathan.gleyze@gmail.com>
2023-10-07 23:26:02 +0200

    [fix] multiple git file

---
+++ src/hooks/offsetScrools.hook.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+/**
+ * useScroolOffsetY Hook
+ * 
+ * Custom React hook to check if the current vertical scroll offset (Y) exceeds a given position.
+ * 
+ * @param {number} pos - The position to check against.
+ * @returns {boolean} - `true` if the current scroll position exceeds `pos`, otherwise `false`.
+ */
+export const useScroolOffsetY = (pos) => {
+    const [isScrolled, setIsScrolled] = useState(false);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+        setIsScrolled(currentScrollY > pos);
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [pos]);
+  
+    return isScrolled;
+  };
