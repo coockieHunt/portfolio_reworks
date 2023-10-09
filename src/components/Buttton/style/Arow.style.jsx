48032aa00a7eb303e238ceeed7a8f714afeb5d32
jonathan gleyze <jonathan.gleyze@gmail.com>
2023-10-10 00:51:29 +0200

    [style] add border

--- src/components/Buttton/style/Arow.style.jsx
+++ src/components/Buttton/style/Arow.style.jsx
@@ -20,6 +20,9 @@ export const ArrowContainer = styled.div`
     transform: rotate(-90deg);
     transition: all 0.3s ease;
     box-sizing: content-box;
+
+    border: 1px solid white;
+
     &:hover{
       box-shadow: rgba(0, 0, 0, 0.17) 0px -5px 5px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
     }
