259583b0f1fdbd933d893cddafe587fa874f1927
jonathan gleyze <jonathan.gleyze@gmail.com>
2023-10-10 00:50:49 +0200

    [add] url list

--- src/config.js
+++ src/config.js
@@ -8,3 +8,13 @@ export const COLOR = {
 export const SCREEN_SIZE = {
     mobile: '850px',
 }
+
+export const URL = {
+    DeviantArt: 'https://www.deviantart.com/coockiehunt',
+    github: 'https://github.com/coockieHunt',
+    linkedin: 'https://www.linkedin.com/in/jonathan-gleyze-173ab7239/',
+    github_hook: 'https://github.com/coockieHunt/custom_hook',
+    ghithudb_portfolio: 'https://github.com/coockieHunt/portfolio',
+    ghithudb_portfolio_rework: 'https://github.com/coockieHunt/portfolio_reworks',
+    ghithudb_game: 'https://github.com/coockieHunt/ck_rp',
+}
