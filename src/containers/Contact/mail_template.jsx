8f55b2e9c0e46b79690745b6bc4db6dcde3f3dd6
jonathan gleyze <jonathan.gleyze@gmail.com>
2023-10-10 23:46:34 +0200

    [fix] missin templete email

--- src/containers/Contact/mail_template.jsx
+++ src/containers/Contact/mail_template.jsx
@@ -1,54 +1,53 @@
-export const Html = (
-    `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Votre e-mail</title>
-    </head>
-    <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0;">
-    
-        <table role="presentation" align="center" width="600" cellpadding="0" cellspacing="0" style="margin: auto; background-color: #121212; color:#fff">
-            <tr>
-                <td style="color: #fff; padding: 10px 30px; display: flex; flex-direction: row; gap: 20px; align-items: center; border-bottom: 1px solid rgba(255, 255, 255, 0.315);">
-                    <img style="height: 40px;" src="https://jonathangleyze.fr/static/media/main_logo.2637bdf3e08873bd8074a64948cc6959.svg"/>
-                    <h1 style="display: block; width: 70%; font-size: 1.3em;">Contact</h1>
-                </td>
-                
-            </tr>
-            <tr >
-                <td style="padding: 25px 30px; background-color: #5843e4;">
-                    <h2 style="color: white; margin: 10px;">Informations :</h2>
-                    <ul style="list-style: none;">
-                        <li><strong>Nom prenom:</strong> somon stre</li>
-                        <li><strong>mail:</strong> somon.stre@gmail.com</li>
-                        <li><strong>object:</strong> objet du mail</li>
-                    </ul>
-                </td>
-            </tr>
-            <tr>
-                <td style="padding: 10px 30px; background-color: #202020;">
-                    <p style="color: #d8d8d8; text-align: center;">
-                        J'espère que vous allez bien. Je me permets de vous contacter car j'ai récemment visité votre site web [Nom du site web] et j'ai été impressionné par son contenu et sa conception.
-    
-                            Je suis [Votre Nom], et je représente [Votre Entreprise/Nom personnel]. Notre entreprise se spécialise dans [Expliquez brièvement votre domaine d'activité ou vos services]. J'ai remarqué que nos domaines d'activité ou nos intérêts sont similaires, et je suis convaincu(e) que nous pourrions potentiellement collaborer ou échanger des idées pour bénéficier mutuellement de nos expertises.
-    
-                            J'aimerais discuter davantage de la possibilité de travailler ensemble, que ce soit par le biais d'un partenariat, d'une collaboration ou d'une quelconque forme de coopération qui pourrait être bénéfique pour nos deux entités.
-    
-                            Si cela vous intéresse, je serais ravi(e) de planifier un appel téléphonique ou une réunion pour discuter plus en détail de nos objectifs communs et des opportunités potentielles. Vous pouvez me joindre à l'adresse électronique [Votre Adresse E-mail] ou par téléphone au [Votre Numéro de Téléphone].
-    
-                            Je vous remercie pour votre temps et votre considération, et j'attends avec impatience la possibilité de discuter de notre collaboration éventuelle.
-    
-                            Cordialement,
-                    </p>
-                </td>
-            </tr>
-        </table>
-    
-    </body>
-    </html>
-    `
-)
-     
+import React from 'react';
 
+export const EmailStructure = ({ content, title, email, FullName }) => {
+    const linkStyle = {
+        color: 'white', // Changer la couleur en noir
+        textDecoration: 'none',
+    };
+
+    return (
+        <html>
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>{title}</title>
+            </head>
+            <body style={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#f4f4f4', margin: 0, padding: 0 }}>
+                <table role="presentation" align="center" cellpadding="0" cellspacing="0" style={{paddingTop: "20px", margin: 'auto', backgroundColor: '#121212', color: '#fff', width: '600px' }}>
+                    <tr>
+                        <td className="header" style={{textAlign: 'center', color: '#fff', padding: '10px 30px', borderBottom: '1px solid rgba(255, 255, 255, 0.315)' }}>
+                            <img className="logo" src="https://jonathangleyze.fr/main_logo.png" style={{ height: '50px' }} alt="Logo" />
+                            <h1 className="title" style={{ fontSize: '1.5em' }}>{title}</h1>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="content" style={{ padding: '25px 30px', backgroundColor: '#5843e4' }}>
+                            <table className="info-list" style={{ listStyle: 'none', color: 'white' , fontSize: '1.2em' }}>
+                                <tr>
+                                    <td><span style={{ fontWeight: 'bold' }}>Nom prénom: </span>{FullName}</td>
+                                </tr>
+                                <tr>
+                                    <td ><span style={{ fontWeight: 'bold' }}>Mail: </span ><a style={linkStyle}>{email}</a></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="message" style={{ padding: '20px 40px', backgroundColor: '#202020' }}>
+                            <p className="message-text" style={{ color: '#d8d8d8', textAlign: 'center', fontSize: '1.2em'}}>
+                                {content}
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="footer" style={{ backgroundColor: '#5843e4', textAlign: 'center' }}>
+                            <span style={{padding: '10px 0', color: 'white', display: "block", width: '100%' }}>06.03.42.02.03</span>
+                            <span style={{padding: '10px 0', color: 'white', display: "block", width: '100%' }}><a style={linkStyle}>pro.jonathan.gleyze@gmail.com</a></span>
+                            <span style={{padding: '10px 0', color: 'white', display: "block", width: '100%' }}><a style={linkStyle}>www.jonathangleyze.fr</a></span>
+                        </td>
+                    </tr>
+                </table>
+            </body>
+        </html>
+    );
+};
