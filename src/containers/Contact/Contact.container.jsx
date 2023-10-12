f83791cd4d8b372ff1f08b23cbbe2f52f2a1a6e6
jonathan gleyze <jonathan.gleyze@gmail.com>
2023-10-13 01:57:36 +0200

    [add] captcha end contact confirm end alert

--- src/containers/Contact/Contact.container.jsx
+++ src/containers/Contact/Contact.container.jsx
@@ -9,6 +9,7 @@ import {
 } from "./Contact.style"
 
 import{
+    COLOR,
     CONTACT_EMAIL
 } from '../../config'
 
@@ -17,6 +18,7 @@ import axios from 'axios';
 import { renderToString } from 'react-dom/server';
 
 import {EmailTemplateContact} from '../../templates/mail.contact.mail'
+import { EmailConfirmTemplate } from '../../templates/mail.confirm.mail';
 
 
 import * as FormComponent from "../../components/Form/From.component"
@@ -29,8 +31,11 @@ import { BiSolidMap } from 'react-icons/bi';
 
 import { useAlert } from '../../context/alert.context';
 
+import {CaptchaComponent} from '../../components/Captacha/Captcha.Component'
+
 export const ContactContainer = ({id}) => {
     const { addAlert } = useAlert();
+    const [isCaptchaValid, setIsCaptchaValid] = useState(false);
 
     let DefaultValue = {
         firstName: '',
@@ -53,90 +58,152 @@ export const ContactContainer = ({id}) => {
     }
 
 
-    function isValidEmail(email) {
+    const isValidEmail = (email) => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
     }
 
-    const handleSubmit = async(e) => {
+
+    const SendEmail =   async(content) => {
+
+        try {
+            const response = await axios.post('https://api.jonathangleyze.fr/sendEmail', content);
+                    
+            if (response.data.success) {
+                return true
+            } else {
+                console.log(response)
+                addAlert(
+                    'Message non evoyer',
+                    "#ffcc00",
+                    4000
+                );
+
+                return false
+
+            }
+          } catch (error) {
+            console.error('Erreur lors de la requête POST vers le serveur:', error);
+            addAlert(
+                "Une erreur s'est produite lors de l'envoi de l'e-mail.",
+                "#cc3300",
+                4000
+            );
+            return false
+        }
+    }
+
+    const CheckData = (output) => {
         if (!output.email || !isValidEmail(output.email)) {
-            addAlert({
-                message: 'Veuillez saisir une adresse email valide.',
-                colorAlert: "#cc3300",
-                delay: 4000
-            });
-            return;
+            addAlert(
+                'Veuillez saisir une adresse email valide.',
+                "#cc3300",
+                4000
+            );
+            return false;
         }
     
         if (!output.firstName) {
-            addAlert({
-                message: 'Veuillez saisir votre prénom.',
-                colorAlert: "#cc3300",
-                delay: 4000
-            });
-            return;
+            addAlert(
+                'Veuillez saisir votre prénom.',
+                "#cc3300",
+                4000
+            );
+            return false;
         }
     
         if (!output.message) {
-            addAlert({
-                message: 'Veuillez saisir votre message.',
-                colorAlert: "#cc3300",
-                delay: 4000
-            });
+            addAlert(
+                'Veuillez saisir votre message.',
+                "#cc3300",
+                4000
+            );
+            return false;
+        }
+
+        return true
+    }
+
+
+    const handleSubmit = async (e) => {
+
+        if (!isCaptchaValid) {
+            addAlert(
+                'Captcha invalide.',
+                "#cc3300",
+                4000
+            );
             return;
-        }
-
-        let subjectFormat = 'Demande de contact de ';
-
-        if (output.firstName) {subjectFormat += `${output.firstName}`;}
-
-        if (output.lastName) {
-            if (output.firstName) {subjectFormat += ' ';}
-            subjectFormat += `${output.lastName}`;
-        }
-
-        subjectFormat = subjectFormat.trim();
-
-        const contentMessage = renderToString(
-            <EmailTemplateContact 
-                content={output.message} 
-                title="Formulaire de contact" 
-                email={output.email} 
-                FullName={subjectFormat}/>
-        );
-
-        const output_format = {
-          to: CONTACT_EMAIL,
-          subject: subjectFormat, 
-          message: contentMessage,
-        };
-
-        try {
-          const response = await axios.post('https://api.jonathangleyze.fr/sendEmail', output_format);
-                  
-          if (response.data.success) {
-            addAlert({
-                message: 'Message bien reçu 👌',
-                delay: 4000
-            });
-            setOutput(DefaultValue); 
-          } else {
-            console.log(response)
-            addAlert({
-                message: 'Message non evoyer',
-                colorAlert: "#ffcc00",
-                delay: 4000
-            });
           }
-        } catch (error) {
-          console.error('Erreur lors de la requête POST vers le serveur:', error);
-          addAlert({
-            message: "Une erreur s'est produite lors de l'envoi de l'e-mail.",
-            colorAlert: "#cc3300",
-            delay: 4000
-        });
+
+        if (CheckData(output)) {
+            // Build subject
+            let subjectFormat = 'Demande de contact de ';
+    
+            if (output.firstName) {
+                subjectFormat += `${output.firstName}`;
+            }
+    
+            if (output.lastName) {
+                if (output.firstName) {
+                    subjectFormat += ' ';
+                }
+                subjectFormat += `${output.lastName}`;
+            }
+    
+            subjectFormat = subjectFormat.trim();
+    
+            // Build mail for the webmaster
+            const contentMessage_webmaster = renderToString(
+            <EmailTemplateContact
+                content={output.message}
+                title="Formulaire de contact"
+                email={output.email}
+                FullName={subjectFormat}/>
+            );
+    
+            const output_format_webmaster = {
+                to: CONTACT_EMAIL,
+                subject: subjectFormat,
+                message: contentMessage_webmaster,
+            };
+    
+            // Send email to the webmaster
+            const webmasterEmailSent = await SendEmail(output_format_webmaster);
+    
+            if (webmasterEmailSent) {
+                const output_format_client = {
+                    to: output.email,
+                    subject: subjectFormat,
+                    message: renderToString(<EmailConfirmTemplate />),
+                };
+    
+                const clientEmailSent = await SendEmail(output_format_client);
+    
+                if (clientEmailSent) {
+                    addAlert(
+                        'Message bien reçu 👌',
+                        COLOR.primary,
+                        3500
+                    );
+                    setOutput(DefaultValue);
+                } else {
+                    addAlert(
+                        "Erreur lors de l'envoi de l'e-mail de confirmation au client.",
+                        "#cc3300",
+                        4000
+                    );
+                }
+            } else {
+                addAlert(
+                    'Message non envoyé',
+                    "#ffcc00",
+                    4000
+                );
+            }
         }
     };
+    
 
     return(
         <div id={id}>
@@ -144,13 +211,12 @@ export const ContactContainer = ({id}) => {
             <Container>
                 {!isMobile ?
                     <Info>
-                        {/* <AlertConponent type="red" message='sqdsqdqsdsqdsd'/> */}
                         <div className="info">
                             <h1>Information</h1>
                             <p>Remplissez ce formulaire, je vous repondrais le plus rapidement possible.</p>
                         
                             <div className="contact">
-                                <Link onClick={() => window.location.replace('https://bobbyhadz.com')}>
+                                <Link onClick={() => window.location.hr('tel:0603420204')}>
                                     <AiFillPhone/> 
                                     <span>+33 6.03.42.02.04</span>
                                 </Link>
@@ -163,8 +229,8 @@ export const ContactContainer = ({id}) => {
                                 <Link>
                                     <BiSolidMap/>
                                     <span
-                                        onClick={() => window.location.replace('https://www.google.com/maps/place/N%C3%AEmes/')}
-                                    >Nîmes</span>
+                                        onClick={() => window.location.href = 'https://www.google.com/maps/place/N%C3%AEmes/'}
+                                    >Nîmes (GARD)</span>
                                 </Link>
                             </div>
                         </div>
@@ -214,13 +280,15 @@ export const ContactContainer = ({id}) => {
                         />
 
                     </FormComponent.Groupe>
+
+                    <CaptchaComponent isCaptchaValid={isCaptchaValid} setIsCaptchaValid={setIsCaptchaValid} />
+
                     <ActionForm>
                             <span onClick={() => {handleReset()}}>Remettre a  zero</span>
                             <Button 
                                 onClick={() => {handleSubmit()}}
                                 icon={<AiOutlineSend/>}
                             >Envoyer</Button>
-
                     </ActionForm>
                 </ContactForm>
             </Container>
---
+++ src/templates/mail.confirm.mail.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export const EmailConfirmTemplate = () => {
+    const linkStyle = {
+        color: 'white',
+        textDecoration: 'none',
+    };
+
+    return (
+        <html>
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Confirmation de réception</title>
+            </head>
+            <body style={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#f4f4f4', margin: 0, padding: 0 }}>
+                <table role="presentation" align="center" cellPadding={0} cellSpacing={0} style={{ paddingTop: "20px", margin: 'auto', backgroundColor: '#121212', color: '#fff', width: '600px' }}>
+                    <tr>
+                        <td className="header" style={{ textAlign: 'center', color: '#fff', padding: '10px 30px', borderBottom: '1px solid rgba(255, 255, 255, 0.315)' }}>
+                            <img className="logo" src="https://jonathangleyze.fr/main_logo.png" style={{ height: '50px' }} alt="Logo" />
+                            <h1 className="title" style={{ fontSize: '1.5em' }}>Confirmation de réception</h1>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="content" style={{ padding: '25px 30px', backgroundColor: '#5843e4' }}>
+                            <table className="info-list" style={{ listStyle: 'none', color: 'white', fontSize: '1.2em' }}>
+                                <tr>
+                                    <td><span style={{ fontWeight: 'bold' }}>Sujet:</span> Demande de contact</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="message" style={{ padding: '20px 40px', backgroundColor: '#202020' }}>
+                            <p className="message-text" style={{ color: '#d8d8d8', textAlign: 'center', fontSize: '1.2em' }}>
+                                Votre message a été envoyé avec succès. Nous vous répondrons bientôt.
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="footer" style={{ backgroundColor: '#5843e4', textAlign: 'center' }}>
+                            <span style={{ padding: '10px 0', color: 'white', display: "block", width: '100%' }}>06.03.42.02.03</span>
+                            <span style={{ padding: '10px 0', color: 'white', display: "block", width: '100%' }}><a style={linkStyle} href="tel:0603420203">pro.jonathan.gleyze@gmail.com</a></span>
+                            <span style={{ padding: '10px 0', color: 'white', display: "block", width: '100%' }}><a style={linkStyle} href="https://www.jonathangleyze.fr">www.jonathangleyze.fr</a></span>
+                        </td>
+                    </tr>
+                </table>
+            </body>
+        </html>
+    );
+};
