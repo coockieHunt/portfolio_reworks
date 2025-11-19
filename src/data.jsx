52d6838c636e0a1f38186feffba4ab60bb3c65f7
coockie_hunt <jonathan.gleyze@gmail.com>
2025-11-19 01:11:57 +0100

    🖌️ Refactor ProcessTimeline: integrate timeline data and enhance component structure

--- src/data.jsx
+++ src/data.jsx
@@ -138,3 +138,11 @@ export const serviceModals = [
         ],
     },
 ];
+
+
+export const TimeLine = [
+    { title: "On se connecte 🔗", content: "Dès réception de votre formulaire de contact et du formulaire de contact détaillé, c'est parti pour un échange téléphonique survolté ! On plonge dans le monde passionnant de votre entreprise et de vos aspirations. Attendez-vous à une avalanche d'idées et d'infos pour créer LE site web qui vous ressemble. 🚀" },
+    { title: "En avant pour le développement ! 👨‍💻", content: "Une fois que vous aurez donné le feu vert sur le devis, c'est parti pour la création de votre site internet ! Je vous tiendrai au courant à chaque étape du processus, vous permettant ainsi de suivre l'évolution de l'interface et de l'expérience utilisateur dès le départ. Attachez vos ceintures, c'est le début d'une aventure digitale palpitante ! 🛠️💻" },
+    { title: "Prêt, set, Go ! 🚀", content: "Une fois que votre site sera dans les starting-blocks et qu'il répondra à toutes vos attentes, je vous délivrerai les clés d'accès pour que vous puissiez vous lancer dans l'aventure en ligne. Accrochez-vous, le succès n'attend que vous ! 🚀🔑" },
+    { title: "Derniers ajustements ! 🔍", content: "Je serai là pour vous guider dans les méandres de vitre site, vous donnant les superpouvoirs nécessaires pour dompter votre site web! Préparez-vous à devenir le maître incontesté de votre royaume en ligne ! 🌟💻" },
+];
