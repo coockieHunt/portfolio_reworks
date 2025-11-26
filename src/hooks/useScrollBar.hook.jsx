6a9a055e016ded2797c3bea2a6f6c8e31ee84927
coockie_hunt <jonathan.gleyze@gmail.com>
2025-11-26 03:40:13 +0100

    🔧 add window on disabled scroolbar for modern web

--- src/hooks/useScrollBar.hook.jsx
+++ src/hooks/useScrollBar.hook.jsx
@@ -1,29 +1,23 @@
 import { useEffect } from 'react';
 
-/**
- * A custom hook that can be used to disable or enable the scrollbars on the body element of a web page.
- *
- * @param {boolean} disableScrollbar - A boolean value indicating whether to disable the scrollbar (true) or enable it (false).
- */
-export const useScrollbar = (disableScrollbar) => {
-	useEffect(() => {
-		const handleScrollbar = () => {
-		if (disableScrollbar) {
-			document.body.style.overflow = 'hidden';
-			document.body.style.touchAction = 'none';
-		} else {
-			document.body.style.overflowY = 'auto';
-			document.body.style.overflowX = 'hidden';
-			document.body.style.touchAction = 'auto';
-		}
-	};
+export const useScrollbar = (disable) => {
+  useEffect(() => {
+    if (disable) {
+      const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
+      
+      document.body.style.paddingRight = `${scrollBarWidth}px`;
+      document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden'; 
+    } else {
+      document.body.style.paddingRight = '0px';
+      document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
+    }
 
-	handleScrollbar();
-	
-	return () => {
-		document.body.style.overflowY = 'auto';
-		document.body.style.overflowX = 'hidden';
-		document.body.style.touchAction = 'auto';
-	};
-	}, [disableScrollbar]);
+    return () => {
+      document.body.style.paddingRight = '0px';
+      document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
+    };
+  }, [disable]);
 };
