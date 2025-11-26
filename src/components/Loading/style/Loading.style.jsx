e96e53c79b1a3e054db5383a465b25606968eb88
coockie_hunt <jonathan.gleyze@gmail.com>
2025-11-26 19:28:19 +0100

    🔧 improved loading style animation fade_text befor slide end font size mobile

--- src/components/Loading/style/Loading.style.jsx
+++ src/components/Loading/style/Loading.style.jsx
@@ -1,6 +1,6 @@
 import styled, { keyframes } from 'styled-components';
+import { SCREEN_SIZE } from '../../../config.jsx';
 
-// 1. Apparition du texte et du fond
 const fadeIn = keyframes`
     0% { opacity: 0; }
     15% { opacity: 1; }
@@ -8,13 +8,11 @@ const fadeIn = keyframes`
     100% { opacity: 0; }
 `;
 
-// 2. Panneau du HAUT : Monte à la fin
 const slideUp = keyframes`
     0%, 80% { transform: translateY(0); }
     100% { transform: translateY(-100%); }
 `;
 
-// 3. Panneau du BAS : Descend à la fin
 const slideDown = keyframes`
     0%, 80% { transform: translateY(0); }
     100% { transform: translateY(100%); }
@@ -30,26 +28,37 @@ export const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    pointer-events: none; /* Bloque les clics au travers */
+    pointer-events: none; 
 `;
 
 export const Content = styled.div`
+    display: inline-flex;
+    justify-content: center;
     z-index: 1000001;
-    font-size: 2rem;
-    font-weight: 800;
-    text-transform: uppercase;
-    letter-spacing: 5px;
+    width: 100%;          
+    text-align: left;      
+    padding: 0 5%;         
+    font-size: 2vw;       
+    line-height: 1.1;      
+    font-weight: 900;      
+    letter-spacing: 2px;   
     color: ${({ $textColor }) => $textColor || 'white'};
-    
-    /* Le texte fade in puis fade out avant l'ouverture */
-    animation: ${fadeIn} ${({ $duration }) => $duration}ms ease-in-out forwards;
+    word-break: break-word; 
+    position: absolute;
+    top: 50%;            
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+
+    @media ( max-width: ${SCREEN_SIZE.mobile}) {font-size: 3.5vw}
+
+    animation: ${fadeIn} ${({ $duration }) => $duration - 300}ms ease-in-out forwards;
 `;
 
 const Panel = styled.div`
     position: absolute;
     left: 0;
     width: 100%;
-    height: 50%;
+    height: 51%; 
     background-color: ${({ $color }) => $color};
     z-index: 1000000;
     will-change: transform;
