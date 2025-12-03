e5b00380c93ac47b8cdeb4bfdb04db4ad653c6e3
coockie_hunt <jonathan.gleyze@gmail.com>
2025-12-03 18:16:31 +0100

    🖌️ Import createHtmlPlugin for dynamic HTML generation in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import { createHtmlPlugin } from 'vite-plugin-html' 
 
 export default defineConfig({
     plugins: [
