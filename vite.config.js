be1a52815b162cf42d848f0f4591825cd1dab6ba
coockie_hunt <jonathan.gleyze@gmail.com>
2025-12-03 18:20:31 +0100

    🖌️ Clean up comments in Vite configuration for clarity and maintainability

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,16 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
-// Note la syntaxe : ({ mode }) => { ... }
 export default defineConfig(({ mode }) => {
-  // 1. On charge les variables d'environnement ICI
   const env = loadEnv(mode, process.cwd());
 
   return {
-    // 2. On retourne la configuration
     plugins: [
       svgr(), 
       react(),
       createHtmlPlugin({
         inject: {
           data: {
-            // Ici 'env' est maintenant connu
             siteUrl: env.VITE_SITE_URL || 'http://localhost:3000',
           },
         },
